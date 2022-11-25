98103b223db463bb98b5107faaf755a9597a9fbe
VeM <vlad.moise@gmail.com>
2022-11-24 22:26:14 -0700

    NeighbourhoodDevelopment class set map when string name is provided

--- src/main/java/com/example/milestone3/NeighbourhoodDevelopment.java
+++ src/main/java/com/example/milestone3/NeighbourhoodDevelopment.java
@@ -2,13 +2,25 @@ package com.example.milestone3;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class NeighbourhoodDevelopment {
 
     HashMap <Integer, Double> development = new HashMap<Integer, Double>();
 
-    NeighbourhoodDevelopment(ArrayList lst, String name){
+    NeighbourhoodDevelopment(List<String[]> data, String neighbourhoodName){
 
+        for (String[] row : data){
+            if(row[2].equals(neighbourhoodName)){
+                if(!development.containsKey(Integer.parseInt(row[0]))){
+                    development.put(Integer.parseInt(row[0]), Double.parseDouble(row[3]));
+                }else {
+                    development.put(Integer.parseInt(row[0]),
+                            development.get(Integer.parseInt(row[0])) + Double.parseDouble(row[3]));
+                }
+            }
+
+        }
 
 
     }
--- src/main/java/com/example/milestone3/test.java
+++ src/main/java/com/example/milestone3/test.java
@@ -1,8 +1,37 @@
 package com.example.milestone3;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.milestone3.Utils.readData;
+
 public class test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
         Neighbourhoods neighbourhoods = new Neighbourhoods();
+
+        List<String[]> developmentData = new ArrayList<>();
+        developmentData = readData("/Datasets/Mature_Neighbourhood_Reinvestment.csv");
+
+        NeighbourhoodDevelopment bellevue = new NeighbourhoodDevelopment(developmentData, "Bellevue");
+
+        //answer for year 2009 should be 86476 = 40000 + 46476
+        System.out.println(bellevue.development.get(2009));
+
+//        for (String[] row : developmentData){
+//            if(row[2].equals("Bellevue")){
+//                System.out.println("found: "+ row[2]);
+//            }
+//
+//        }
+
+//        for (int i = 0; i < 100; i++) {
+//
+//            System.out.println(Arrays.toString(developmentData.get(i)));
+//        }
     }
 }
